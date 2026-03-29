576628fe85382184a4cbbe1316bd877afa52718b
NitonMeira <niltonprudys@gmail.com>
2026-03-29 16:41:42 -0300

    Feat: saving unifield data

--- src/ingest.py
+++ src/ingest.py
@@ -1,7 +1,8 @@
 import pandas as pd
 from pathlib import Path
 
-DATA_PATH = Path("data/raw")
+RAW = Path("data/raw")
+PROCESSED = Path("data/processed")
 
 files = [
     "EXP_2021.csv",
@@ -15,8 +16,10 @@ dfs = []
 
 for file in files:
     print(f"Loading {file}")
-    df = pd.read_csv(DATA_PATH / file, sep=";")
+    df = pd.read_csv(RAW / file, sep=";")
     dfs.append(df)
 
 exported = pd.concat(dfs, ignore_index=True)
-print(exported.shape)
+print(f"Exported lines: {exported.shape}")
+print(exported.head)
+exported.to_csv(PROCESSED / "unified_data.csv", index=False)
